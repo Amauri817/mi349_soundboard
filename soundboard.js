9b6d461e83c53304a962be2097bec2feb126be87
Amauri <brownamauri@icloud.com>
2021-06-20 22:13:12 -0400

    new

--- soundboard.js
+++ soundboard.js
@@ -1,25 +1,26 @@
-alert('Welcome to my website!');
-const sounds = ['Applause','Ahem', 'Advice', 'Adequetely'];
+alert ('Welcome to my website');
+const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
 
-sounds.forEach(sound => {
-    const btn = document.createElement('button');
-    btn.classList.add('btn');
+sounds.forEach((sound) => {
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
 
-    btn.innerText = sound;
+  btn.innerText = sound;
 
-    btn.addEventListener('click', () =>{
-        stopSongs();
-        document.getElementById(sound).play();
-    });
+  btn.addEventListener("click", () => {
+    stopSongs();
 
-    document.getElementById('buttons').appendChild(btn);
+    document.getElementById(sound).play();
+  });
+
+  document.getElementById("buttons").appendChild(btn);
 });
 
 function stopSongs() {
-    sounds.forEach((sound) => {
-        const song = document.getElementById(sound)
+  sounds.forEach((sound) => {
+    const song = document.getElementById(sound);
 
-        song.onpause();
-        song.currentTime = 0;
-    });
+    song.pause();
+    song.currentTime = 0;
+  });
 }
